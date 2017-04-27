3bcb5fd3dd99e3b1c3f4d7fb634593bcf94a7b08
Anton Kostash <sha1md5antonkostash@gmail.com>
2017-04-27 10:46:51 +0100

    Modified provision scripts:  - nano.sh - added variable defininition with filepath to configuration file

--- provision/nano.sh
+++ provision/nano.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sed -i "s/# set tabstospaces/set tabstospaces/g" /etc/nanorc
-sed -i "s/# set tabsize 8/set tabsize 4/g" /etc/nanorc
-sed -i "s/# set softwrap/set softwrap/g" /etc/nanorc
-sed -i "s/# set fill -8/set fill -4/g" /etc/nanorc
+FILEPATH="/etc/nanorc"
+
+sed -i "s/# set tabstospaces/set tabstospaces/g" $FILEPATH
+sed -i "s/# set tabsize 8/set tabsize 4/g" $FILEPATH
+sed -i "s/# set softwrap/set softwrap/g" $FILEPATH
+sed -i "s/# set fill -8/set fill -4/g" $FILEPATH
 
